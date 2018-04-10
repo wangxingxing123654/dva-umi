96ec0ddf580bd321f4c898d336dc4ba60627201b
wangxingixng <1129757968@qq.com>
2018-04-10 09:16:08 +0800

    crud操作

--- src/pages/users/components/User.js
+++ src/pages/users/components/User.js
@@ -1,13 +1,34 @@
 import { connect } from 'dva';
-import { Table, Pagination, Popconfirm } from 'antd';
+import { Table, Pagination, Popconfirm, Button } from 'antd';
 import styles from './User.css';
 import { PAGE_SIZE } from '../constants';
-
-function Users({ list: dataSource, total, page: current }) {
+import { routerRedux } from 'dva/router';
+import UserModal from './UserModal';
+function Users({ dispatch,  list: dataSource,loading,  total, page: current }) {
   function deleteHandler(id) {
-    console.warn(`TODO: ${id}`);
+    dispatch({
+      type: 'users/remove',
+      payload: id,
+    });
   }
-
+  function pageChangeHandler(page) {
+    dispatch(routerRedux.push({
+     pathname: '/users',
+     query: { page },
+    }));
+  }
+  function editHandler(id, values) {
+    dispatch({
+      type: 'users/patch',
+      payload: { id, values },
+    });
+  }
+  function createHandler(values) {
+    dispatch({
+      type: 'users/create',
+      payload: values,
+    });
+  }
   const columns = [
     {
       title: 'Name',
@@ -28,10 +49,12 @@ function Users({ list: dataSource, total, page: current }) {
     {
       title: 'Operation',
       key: 'operation',
-      render: (text, { id }) => (
+      render: (text, record) => (
         <span className={styles.operation}>
-          <a href="">Edit</a>
-          <Popconfirm title="Confirm to delete?" onConfirm={deleteHandler.bind(null, id)}>
+          <UserModal record={record} onOk={editHandler.bind(null, record.id)}>
+            <a>Edit</a>
+          </UserModal>
+          <Popconfirm title="Confirm to delete?" onConfirm={deleteHandler.bind(null, record.id)}>
             <a href="">Delete</a>
           </Popconfirm>
         </span>
@@ -42,17 +65,24 @@ function Users({ list: dataSource, total, page: current }) {
   return (
     <div className={styles.normal}>
       <div>
+      <div className={styles.create}>
+          <UserModal record={{}} onOk={createHandler}>
+            <Button type="primary">Create User</Button>
+          </UserModal>
+        </div>
         <Table
           columns={columns}
           dataSource={dataSource}
           rowKey={record => record.id}
           pagination={false}
+          loading={loading}
         />
         <Pagination
           className="ant-table-pagination"
           total={total}
           current={current}
           pageSize={PAGE_SIZE}
+          onChange={pageChangeHandler}
         />
       </div>
     </div>
@@ -65,6 +95,7 @@ function mapStateToProps(state) {
     list,
     total,
     page,
+    loading: state.loading.models.users,
   };
 }
 
--- src/pages/users/models/users.js
+++ src/pages/users/models/users.js
@@ -17,6 +17,16 @@ export default {
       const { data, headers } = yield call(usersService.fetch, { page });
       yield put({ type: 'save', payload: { data, total: parseInt(headers['x-total-count'], 10),  page: parseInt(page, 10), } });
     },
+    *remove({ payload: id }, { call, put, select }) {
+      yield call(usersService.remove, id);
+      const page = yield select(state => state.users.page);
+      yield put({ type: 'fetch', payload: { page } });
+    },
+    *patch({ payload: { id, values } }, { call, put, select }) {
+      yield call(usersService.patch, id, values);
+      const page = yield select(state => state.users.page);
+      yield put({ type: 'fetch', payload: { page } });
+    },
   },
   subscriptions: {
     setup({ dispatch, history }) {
